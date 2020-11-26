9fe5d065750b04cbc835385cfea479c36b31a8cf
Cácio Lucas <40599933+caciolucas@users.noreply.github.com>
2020-11-26 12:03:18 -0300

    New deaths array(d) and prettier plot

--- Trabalho Final/plot.py
+++ Trabalho Final/plot.py
@@ -17,6 +17,7 @@ import matplotlib.pyplot as plt
 s = []
 i = []
 r = []
+d = []
 t = []
 x=0
 with open(sys.argv[1],'r') as csvfile:
@@ -26,13 +27,16 @@ with open(sys.argv[1],'r') as csvfile:
             s.append(float(row[0]))
             i.append(float(row[1]))
             r.append(float(row[2]))
-            t.append(float(row[3]))
+            d.append(float(row[3]))
+            t.append(float(row[4]))
         else:
             x+=1
 
-plt.plot(t,s, label='S')
-plt.plot(t,i, label='I')
-plt.plot(t,r, label='R')
+plt.plot(t,s, linestyle='-', markerfacecolor='g', label='Sucetíveis')
+plt.plot(t,i, linestyle='-.', markerfacecolor='#FF8C00', label='Infectados')
+plt.plot(t,r, linestyle=':', markerfacecolor='r', label='Removidos')
+plt.plot(t,d, linestyle='--', markerfacecolor='k', label='Mortos')
+
 plt.xlabel('Horas')
 plt.ylabel('Pessoas')
 plt.title('Grafico SIR')
